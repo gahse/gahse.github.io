8bbf3166b65a4a73528c8660181bfc9f2f283775
Ghufran Ahmed <Hashmi>
2016-09-18 01:55:08 +0500

    without images testing

--- js/init.js
+++ js/init.js
@@ -201,31 +201,31 @@ function loadContent(){
     });
   },3000);
 
-  setTimeout(function(){
-    $.get("content/portfolio.html", function(data, status){
-      if(status==="success"){
-        $("#portfolio").html(data);
+//   setTimeout(function(){
+//     $.get("content/portfolio.html", function(data, status){
+//       if(status==="success"){
+//         $("#portfolio").html(data);
 
         
-/*----------------------------------------------------*/
-/*  Modal Popup
-------------------------------------------------------*/
+// /*----------------------------------------------------*/
+// /*  Modal Popup
+// ------------------------------------------------------*/
 
-    $('.item-wrap a').magnificPopup({
+//     $('.item-wrap a').magnificPopup({
 
-       type:'inline',
-       fixedContentPos: false,
-       removalDelay: 200,
-       showCloseBtn: false,
-       mainClass: 'mfp-fade'
+//        type:'inline',
+//        fixedContentPos: false,
+//        removalDelay: 200,
+//        showCloseBtn: false,
+//        mainClass: 'mfp-fade'
 
-    });
+//     });
 
-    $(document).on('click', '.popup-modal-dismiss', function (e) {
-        e.preventDefault();
-        $.magnificPopup.close();
-    });
-      }
-    });
-  },3000);
+//     $(document).on('click', '.popup-modal-dismiss', function (e) {
+//         e.preventDefault();
+//         $.magnificPopup.close();
+//     });
+//       }
+//     });
+//   },3000);
 }
